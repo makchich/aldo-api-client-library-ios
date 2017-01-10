5e3396e706f0f3f08eb68df6b175f95c75ec14cc
makchich <m.akchich@student.tudelft.nl>
2017-01-10 08:36:34 +0100

    Add Regex.swift to the Aldo Pod.
    This file contains the Regex struct used to compare the request command with the AldoRequest regex value.

---
+++ Aldo/Regex.swift
@@ -0,0 +1,35 @@
+//from NSHipster - http://nshipster.com/swift-literal-convertible/
+public struct Regex {
+    let pattern: String
+    let options: NSRegularExpression.Options!
+    
+    private var matcher: NSRegularExpression {
+        return try! NSRegularExpression(pattern: self.pattern, options: self.options)
+    }
+    
+    public init(pattern: String, options: NSRegularExpression.Options = []) {
+        self.pattern = pattern
+        self.options = options
+    }
+    
+    public func match(string: String, options: NSRegularExpression.MatchingOptions = []) -> Bool {
+        return self.matcher.numberOfMatches(in: string, options: options, range: NSMakeRange(0, string.utf16.count)) != 0
+    }
+}
+
+//from Les Stroud http://www.lesstroud.com/swift-using-regex-in-switch-statements/
+public protocol RegularExpressionMatchable {
+    func match(regex: Regex) -> Bool
+}
+
+//from Les Stroud http://www.lesstroud.com/swift-using-regex-in-switch-statements/
+extension String: RegularExpressionMatchable {
+    public func match(regex: Regex) -> Bool {
+        return regex.match(string: self)
+    }
+}
+
+//from Les Stroud http://www.lesstroud.com/swift-using-regex-in-switch-statements/
+public func ~=<T: RegularExpressionMatchable>(pattern: Regex, matchable: T) -> Bool {
+    return matchable.match(regex: pattern)
+}
