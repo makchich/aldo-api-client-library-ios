5feeedf93c293454c4c21bab26390b3df5d1a0be
makchich <m.akchich@student.tudelft.nl>
2017-01-19 22:12:38 +0100

    Empty Application without a demo of the Library.

--- Example/Aldo/ViewController.swift
+++ Example/Aldo/ViewController.swift
@@ -3,7 +3,7 @@
 //  Aldo
 //
 //  Created by makchich on 01/03/2017.
-//  Copyright (c) 2017 makchich. All rights reserved.
+//  Copyright (c) 2017 Team Aldo. All rights reserved.
 //
 
 import UIKit
@@ -13,21 +13,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let username: String = "iPhone_Simulator"
-        
-        Aldo.setHostAddress(address: "127.0.0.1")
-        Aldo.requestAuthToken()
-        Aldo.createSession(username: username)
-        Aldo.joinSession(username: username, token: "token")
-        Aldo.requestSessionInfo()
-        Aldo.requestSessionPlayers()
-        Aldo.changeSessionState(newState: AldoSession.State.PAUSE)
-        Aldo.deleteSession()
-        Aldo.requestPlayerInfo()
-        Aldo.requestDevicePlayers()
-        Aldo.updateUsername(username: "iPhone_Simulator_update")
     }
 
 }
-
