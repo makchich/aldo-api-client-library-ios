a99db961ea97dc45e812fd3ad163752739ae91ed
makchich <m.akchich@student.tudelft.nl>
2017-01-21 19:30:12 +0100

    Add swiftlint config comment to disable warnings.

--- Aldo/Regex.swift
+++ Aldo/Regex.swift
@@ -2,18 +2,23 @@
 public struct Regex {
     let pattern: String
     let options: NSRegularExpression.Options!
-    
+
     private var matcher: NSRegularExpression {
+        // swiftlint:disable:next force_try
         return try! NSRegularExpression(pattern: self.pattern, options: self.options)
+        // swiftlint:disable:previous force_try
     }
-    
+
     public init(pattern: String, options: NSRegularExpression.Options = []) {
         self.pattern = pattern
         self.options = options
     }
-    
+
     public func match(string: String, options: NSRegularExpression.MatchingOptions = []) -> Bool {
-        return self.matcher.numberOfMatches(in: string, options: options, range: NSMakeRange(0, string.utf16.count)) != 0
+        return self.matcher.numberOfMatches(
+            in: string, options: options,
+            range: NSMakeRange(0, string.utf16.count) //swiftlint:disable:this legacy_constructor
+        ) != 0
     }
 }
 
